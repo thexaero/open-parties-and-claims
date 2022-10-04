c6f13f70f05a69a1a416b6eef824e32487aba31d
thexaero <xaero1996@gmail.com>
2022-10-04 14:39:07 +0300

    fixed party ally removal not being synced

--- Common/src/main/java/xaero/pac/common/packet/parties/ClientboundPartyAllyPacket.java
+++ Common/src/main/java/xaero/pac/common/packet/parties/ClientboundPartyAllyPacket.java
@@ -71,7 +71,7 @@ public class ClientboundPartyAllyPacket extends LazyPacket<LazyPacket.Encoder<Cl
 				if(allyName.length() > 512)
 					return null;
 				String allyDefaultName = tag.getString("dn");
-				if(allyDefaultName.isEmpty() || allyDefaultName.length() > 512)
+				if(allyDefaultName.length() > 512)
 					return null;
 				String actionString = tag.getString("a");
 				if(actionString.isEmpty() || actionString.length() > 128)
