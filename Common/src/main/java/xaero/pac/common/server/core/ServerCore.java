023ea5de583863b86ddb038379c0c882fde0449d
thexaero <xaero1996@gmail.com>
2025-02-26 11:04:12 +0200

    fixes for 1.19.4

--- Common/src/main/java/xaero/pac/common/server/core/ServerCore.java
+++ Common/src/main/java/xaero/pac/common/server/core/ServerCore.java
@@ -33,6 +33,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
