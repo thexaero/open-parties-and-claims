f0885fdb2a26d23b66ea3208980bd1cd9df628cf
thexaero <xaero1996@gmail.com>
2025-07-25 09:56:12 +0300

    fixed crash when interacting with armor stand

--- Common/src/main/java/xaero/pac/common/server/core/ServerCore.java
+++ Common/src/main/java/xaero/pac/common/server/core/ServerCore.java
@@ -28,6 +28,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.Connection;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.game.ServerboundInteractPacket;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
@@ -62,6 +63,7 @@ import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemp
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
 import xaero.pac.OpenPartiesAndClaims;
 import xaero.pac.common.claims.player.IPlayerChunkClaim;
 import xaero.pac.common.claims.player.IPlayerClaimPosList;
@@ -90,6 +92,7 @@ import xaero.pac.common.server.core.accessor.IServerCommonPacketListenerImpl;
 import xaero.pac.common.server.parties.party.IServerParty;
 import xaero.pac.common.server.world.ServerLevelHelper;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.lang.reflect.Field;
 import java.util.Iterator;
@@ -544,6 +547,31 @@ public class ServerCore {
 			return false;
 		Entity cart2 = world.getEntity(cartId2);
 		return cart2 == null || !serverData.getChunkProtection().onEntityInteraction(serverData, null, player, cart2, heldItem, null, false, false, true);
+	}
+
+	private static InteractionHand ENTITY_INTERACTION_HAND;
+
+	//still here on 1.20.2 because NeoForge doesn't fire the server entity interact specific event
+	public static boolean canInteract(ServerGamePacketListenerImpl packetListener, ServerboundInteractPacket packet){
+		ENTITY_INTERACTION_HAND = null;
+		packet.dispatch(new ServerboundInteractPacket.Handler() {
+			@Override
+			public void onInteraction(@Nonnull InteractionHand interactionHand) {}
+			@Override
+			public void onInteraction(@Nonnull InteractionHand interactionHand, @Nonnull Vec3 vec3) {
+				ENTITY_INTERACTION_HAND = interactionHand;
+			}
+			@Override
+			public void onAttack() {}
+		});
+		if(ENTITY_INTERACTION_HAND == null)//not specific interaction
+			return true;
+		ServerPlayer player = packetListener.player;
+		ServerLevel level = player.serverLevel();
+		final Entity entity = packet.getTarget(level);
+		if(entity == null)
+			return true;
+		return !OpenPartiesAndClaims.INSTANCE.getCommonEvents().onInteractEntitySpecific(player, entity, ENTITY_INTERACTION_HAND);
 	}
 
 	public static boolean replaceEntityIsInvulnerable(boolean actual, DamageSource damageSource, Entity entity){
