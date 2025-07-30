b4caf69ee7cd965c754437f8a781918c345d5252
thexaero <xaero1996@gmail.com>
2025-07-30 11:48:49 +0300

    replaced create coremod injections with mixins

---
+++ Fabric/src/main/java/xaero/pac/common/mixin/create/MixinFabricTrainRelocationPacket.java
@@ -0,0 +1,50 @@
+/*
+ * Open Parties and Claims - adds chunk claims and player parties to Minecraft
+ * Copyright (C) 2023-2025, Xaero <xaero1996@gmail.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU Lesser General Public License
+ * (LGPL-3.0-only) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received copies of the GNU Lesser General Public License
+ * and the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package xaero.pac.common.mixin.create;
+
+import com.simibubi.create.content.trains.entity.TrainRelocationPacket;
+import com.simibubi.create.foundation.networking.SimplePacketBase;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xaero.pac.common.server.core.ServerCore;
+
+@Mixin(TrainRelocationPacket.class)
+public class MixinFabricTrainRelocationPacket {
+
+	@Shadow(remap = false)
+	int entityId;
+
+	@Shadow
+	BlockPos pos;
+
+	@Inject(method = "lambda$handle$2", remap = false, at = @At("HEAD"), cancellable = true)
+	public void onHandle(SimplePacketBase.Context context, CallbackInfo ci){
+		ServerPlayer player = context.getSender();
+		if (player == null)
+			return;
+		if(!ServerCore.isCreateTrainRelocationPacketAllowed(entityId, pos, player))
+			ci.cancel();
+	}
+
+}
