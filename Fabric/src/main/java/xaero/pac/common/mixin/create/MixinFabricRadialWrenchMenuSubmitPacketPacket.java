570e6574962e77b38e91afc9496b79459c3751ce
thexaero <xaero1996@gmail.com>
2025-08-02 14:02:31 +0300

    added create wrench radial menu protection, fixes #685

---
+++ Fabric/src/main/java/xaero/pac/common/mixin/create/MixinFabricRadialWrenchMenuSubmitPacketPacket.java
@@ -0,0 +1,43 @@
+/*
+ * Open Parties and Claims - adds chunk claims and player parties to Minecraft
+ * Copyright (C) 2025, Xaero <xaero1996@gmail.com> and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU Lesser General Public License
+ * (LGPL-3.0-only) as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received copies of the GNU Lesser General Public License
+ * and the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package xaero.pac.common.mixin.create;
+
+import com.simibubi.create.content.contraptions.wrench.RadialWrenchMenuSubmitPacket;
+import com.simibubi.create.foundation.networking.SimplePacketBase;
+import net.minecraft.core.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xaero.pac.common.server.core.ServerCore;
+
+@Mixin(RadialWrenchMenuSubmitPacket.class)
+public class MixinFabricRadialWrenchMenuSubmitPacketPacket {
+
+	@Shadow
+	private BlockPos blockPos;
+
+	@Inject(method = "lambda$handle$0", remap = false, at = @At("HEAD"), cancellable = true)
+	public void onActivate(SimplePacketBase.Context ctx, CallbackInfo ci){
+		if(!ServerCore.isCreateBlockPacketAllowed(blockPos, ctx.getSender()))
+			ci.cancel();
+	}
+
+}
