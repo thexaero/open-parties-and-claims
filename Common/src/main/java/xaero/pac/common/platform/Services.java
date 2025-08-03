9967853722aabc80211a6bd3c5022ee8a63095dd
thexaero <xaero1996@gmail.com>
2025-08-03 12:32:27 +0300

    added version range support for mixins

--- Common/src/main/java/xaero/pac/common/platform/Services.java
+++ Common/src/main/java/xaero/pac/common/platform/Services.java
@@ -25,7 +25,7 @@ import java.util.ServiceLoader;
 
 public class Services {
 
-	public static final IPlatformHelper PLATFORM = load(IPlatformHelper.class);
+	public static final IPlatformHelper<?, ?> PLATFORM = load(IPlatformHelper.class);
 
 	public static <T> T load(Class<T> clazz) {
 
