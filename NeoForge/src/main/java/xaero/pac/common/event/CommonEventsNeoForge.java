79a5c5afeb1a861afd42f7c8d21a2a247cb68ae0
thexaero <xaero1996@gmail.com>
2025-09-15 11:25:55 +0300

    fixed crash when converting a villager on neoforge

--- NeoForge/src/main/java/xaero/pac/common/event/CommonEventsNeoForge.java
+++ NeoForge/src/main/java/xaero/pac/common/event/CommonEventsNeoForge.java
@@ -261,6 +261,8 @@ public class CommonEventsNeoForge extends CommonEvents {
 
 	@SubscribeEvent(priority = EventPriority.HIGHEST)
 	public void onMobCheckSpawn(MobSpawnEvent.FinalizeSpawn event){
+		if(event.getEntity().isAddedToWorld())
+			return;
 		if(super.onMobSpawn(event.getEntity(), event.getX(), event.getY(), event.getZ(), event.getSpawnType())) {
 			event.setSpawnCancelled(true);//won't be spawned
 			event.setCanceled(true);//won't call finalizeSpawn
