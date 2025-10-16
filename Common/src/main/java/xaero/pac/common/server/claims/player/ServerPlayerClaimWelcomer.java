f2471f037e9566dc64a209746cbdf3c58fa24311
thexaero <xaero1996@gmail.com>
2025-10-16 11:25:49 +0300

    added option to disable claim welcomer, closes #526

--- Common/src/main/java/xaero/pac/common/server/claims/player/ServerPlayerClaimWelcomer.java
+++ Common/src/main/java/xaero/pac/common/server/claims/player/ServerPlayerClaimWelcomer.java
@@ -35,6 +35,7 @@ import xaero.pac.common.server.IServerData;
 import xaero.pac.common.server.claims.IServerClaimsManager;
 import xaero.pac.common.server.claims.IServerDimensionClaimsManager;
 import xaero.pac.common.server.claims.IServerRegionClaims;
+import xaero.pac.common.server.config.ServerConfig;
 import xaero.pac.common.server.parties.party.IServerParty;
 import xaero.pac.common.server.player.config.IPlayerConfig;
 import xaero.pac.common.server.player.config.PlayerConfig;
@@ -51,44 +52,48 @@ public class ServerPlayerClaimWelcomer {
 		IPlayerChunkClaim lastClaimCheck = mainCap.getLastClaimCheck();
 		IServerClaimsManager<?, ?, ?> claimsManager = serverData.getServerClaimsManager();
 		IPlayerChunkClaim currentClaim = claimsManager.get(player.getLevel().dimension().location(), player.chunkPosition());
-		if (!Objects.equals(lastClaimCheck, currentClaim)) {
-			AdaptiveLocalizer adaptiveLocalizer = serverData.getAdaptiveLocalizer();
-			UUID currentClaimId = currentClaim == null ? null : currentClaim.getPlayerId();
-			IPlayerClaimInfo<IPlayerDimensionClaims<IPlayerClaimPosList>> playerClaimInfo = currentClaim == null ? null : serverData.getServerClaimsManager().getPlayerInfo(currentClaimId);
-			boolean isOwner = !mainCap.isClaimsNonallyMode() && currentClaim != null && Objects.equals(currentClaimId, player.getUUID());
-			boolean hasAccess = isOwner || serverData.getChunkProtection().hasChunkAccess(serverData.getChunkProtection().getClaimConfig(serverData.getPlayerConfigs(), currentClaim), player, null);
-
-			IPlayerConfig claimConfig = serverData.getChunkProtection().getClaimConfig(serverData.getPlayerConfigs(), currentClaim);
-			String customName = claimConfig.getEffective(PlayerConfigOptions.CLAIMS_NAME);
-			int claimColor = claimConfig.getEffective(PlayerConfigOptions.CLAIMS_COLOR);
-			MutableComponent subTitleText;
-			if (playerClaimInfo == null)
-				subTitleText = adaptiveLocalizer.getFor(player, customName == null || customName.isEmpty() ? "gui.xaero_pac_title_entered_wilderness" : customName);
-			else {
-				MutableComponent properDesc;
-				Component forceloadedComponent = currentClaim.isForceloadable() ? adaptiveLocalizer.getFor(player, "gui.xaero_pac_marked_for_forceload") : new TextComponent("");
-				if (Objects.equals(currentClaimId, PlayerConfig.SERVER_CLAIM_UUID))
-					properDesc = adaptiveLocalizer.getFor(player, "gui.xaero_pac_title_entered_server_claim", forceloadedComponent);
-				else if (Objects.equals(currentClaimId, PlayerConfig.EXPIRED_CLAIM_UUID))
-					properDesc = adaptiveLocalizer.getFor(player, "gui.xaero_pac_title_entered_expired_claim", forceloadedComponent);
-				else
-					properDesc = adaptiveLocalizer.getFor(player, "gui.xaero_pac_title_entered_claim", playerClaimInfo.getPlayerUsername(), forceloadedComponent);
-				if (customName != null && !customName.isEmpty()) {
-					subTitleText = new TextComponent(customName + " - ");
-					subTitleText.getSiblings().add(properDesc);
-				} else
-					subTitleText = properDesc;
-			}
-			subTitleText = subTitleText.withStyle(s -> s.withColor(isOwner ? ChatFormatting.DARK_GREEN : hasAccess ? ChatFormatting.GOLD : ChatFormatting.DARK_RED));
-
-			MutableComponent subTitle = new TextComponent("□ ").withStyle(s -> s.withColor(claimColor));
-			subTitle.getSiblings().add(subTitleText);
-			subTitle.getSiblings().add(new TextComponent(" □").withStyle(s -> s.withColor(claimColor)));
-			ClientboundSetActionBarTextPacket packet = new ClientboundSetActionBarTextPacket(subTitle);
-			player.connection.send(packet);
-
+		if (Objects.equals(lastClaimCheck, currentClaim))
+			return;
+		if(!ServerConfig.CONFIG.claimWelcomeMessages.get()){
 			mainCap.setLastClaimCheck(currentClaim);
+			return;
 		}
+		AdaptiveLocalizer adaptiveLocalizer = serverData.getAdaptiveLocalizer();
+		UUID currentClaimId = currentClaim == null ? null : currentClaim.getPlayerId();
+		IPlayerClaimInfo<IPlayerDimensionClaims<IPlayerClaimPosList>> playerClaimInfo = currentClaim == null ? null : serverData.getServerClaimsManager().getPlayerInfo(currentClaimId);
+		boolean isOwner = !mainCap.isClaimsNonallyMode() && currentClaim != null && Objects.equals(currentClaimId, player.getUUID());
+		boolean hasAccess = isOwner || serverData.getChunkProtection().hasChunkAccess(serverData.getChunkProtection().getClaimConfig(serverData.getPlayerConfigs(), currentClaim), player, null);
+
+		IPlayerConfig claimConfig = serverData.getChunkProtection().getClaimConfig(serverData.getPlayerConfigs(), currentClaim);
+		String customName = claimConfig.getEffective(PlayerConfigOptions.CLAIMS_NAME);
+		int claimColor = claimConfig.getEffective(PlayerConfigOptions.CLAIMS_COLOR);
+		MutableComponent subTitleText;
+		if (playerClaimInfo == null)
+			subTitleText = adaptiveLocalizer.getFor(player, customName == null || customName.isEmpty() ? "gui.xaero_pac_title_entered_wilderness" : customName);
+		else {
+			MutableComponent properDesc;
+			Component forceloadedComponent = currentClaim.isForceloadable() ? adaptiveLocalizer.getFor(player, "gui.xaero_pac_marked_for_forceload") : new TextComponent("");
+			if (Objects.equals(currentClaimId, PlayerConfig.SERVER_CLAIM_UUID))
+				properDesc = adaptiveLocalizer.getFor(player, "gui.xaero_pac_title_entered_server_claim", forceloadedComponent);
+			else if (Objects.equals(currentClaimId, PlayerConfig.EXPIRED_CLAIM_UUID))
+				properDesc = adaptiveLocalizer.getFor(player, "gui.xaero_pac_title_entered_expired_claim", forceloadedComponent);
+			else
+				properDesc = adaptiveLocalizer.getFor(player, "gui.xaero_pac_title_entered_claim", playerClaimInfo.getPlayerUsername(), forceloadedComponent);
+			if (customName != null && !customName.isEmpty()) {
+				subTitleText = new TextComponent(customName + " - ");
+				subTitleText.getSiblings().add(properDesc);
+			} else
+				subTitleText = properDesc;
+		}
+		subTitleText = subTitleText.withStyle(s -> s.withColor(isOwner ? ChatFormatting.DARK_GREEN : hasAccess ? ChatFormatting.GOLD : ChatFormatting.DARK_RED));
+
+		MutableComponent subTitle = new TextComponent("□ ").withStyle(s -> s.withColor(claimColor));
+		subTitle.getSiblings().add(subTitleText);
+		subTitle.getSiblings().add(new TextComponent(" □").withStyle(s -> s.withColor(claimColor)));
+		ClientboundSetActionBarTextPacket packet = new ClientboundSetActionBarTextPacket(subTitle);
+		player.connection.send(packet);
+
+		mainCap.setLastClaimCheck(currentClaim);
 	}
 
 }
