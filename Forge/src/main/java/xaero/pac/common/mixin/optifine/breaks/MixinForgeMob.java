577951b60f6d8cd79e6775a18c5e3e8ed3f8e0c1
thexaero <xaero1996@gmail.com>
2026-02-17 09:43:28 +0200

    fix for 1.19.2

--- Forge/src/main/java/xaero/pac/common/mixin/optifine/breaks/MixinForgeMob.java
+++ Forge/src/main/java/xaero/pac/common/mixin/optifine/breaks/MixinForgeMob.java
@@ -18,6 +18,7 @@
 
 package xaero.pac.common.mixin.optifine.breaks;
 
+import net.minecraft.core.Vec3i;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.item.ItemEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -33,7 +34,7 @@ import java.util.Iterator;
 public class MixinForgeMob {
 
 	@Inject(method = "aiStep", locals = LocalCapture.CAPTURE_FAILSOFT, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Mob;pickUpItem(Lnet/minecraft/world/entity/item/ItemEntity;)V"), cancellable = true)
-	public void onAiStepItemPickup(CallbackInfo ci, Iterator var2, ItemEntity itemEntity){//different from fabric parameters
+	public void onAiStepItemPickup(CallbackInfo ci, Vec3i vec3i, Iterator var2, ItemEntity itemEntity){//different from fabric parameters
 		if(ServerCore.onMobItemPickup(itemEntity, (Mob)(Object)this))
 			ci.cancel();
 	}
