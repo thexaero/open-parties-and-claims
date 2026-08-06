1c78c8cd4af85dcdd2499c170417a6fa80f8d8ae
thexaero <xaero1996@gmail.com>
2026-08-06 14:08:23 +0300

    fixed regular players being suggested the "clear for" command

--- Common/src/main/java/xaero/pac/common/server/claims/command/ClaimsClearCommand.java
+++ Common/src/main/java/xaero/pac/common/server/claims/command/ClaimsClearCommand.java
@@ -95,16 +95,16 @@ public class ClaimsClearCommand {
 		LiteralArgumentBuilder<CommandSourceStack> targetNoConfirmCommand = Commands.literal(ClaimsCommandRegister.COMMAND_PREFIX).requires(c -> ServerConfig.CONFIG.claimsEnabled.get())
 				.then(Commands.literal("clear")
 				.then(Commands.literal("for")
-				.then(Commands.argument("profile", GameProfileArgument.gameProfile())
 				.requires(targetRequirement)
+				.then(Commands.argument("profile", GameProfileArgument.gameProfile())
 				.suggests(targetSuggestions)
 				.executes(getExecutor(false, false)))));
 		dispatcher.register(targetNoConfirmCommand);
 		LiteralArgumentBuilder<CommandSourceStack> targetCommand = Commands.literal(ClaimsCommandRegister.COMMAND_PREFIX).requires(c -> ServerConfig.CONFIG.claimsEnabled.get())
 				.then(Commands.literal("clear")
 				.then(Commands.literal("for")
-				.then(Commands.argument("profile", GameProfileArgument.gameProfile())
 				.requires(targetRequirement)
+				.then(Commands.argument("profile", GameProfileArgument.gameProfile())
 				.suggests(targetSuggestions)
 				.then(Commands.literal("confirm")
 				.executes(getExecutor(true, false))))));
